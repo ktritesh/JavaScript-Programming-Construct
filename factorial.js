0be0f68b19c8a2eaa7492aff0b123526405421b1
ktritsh7 <ktriesh7@gmail.com>
2021-07-23 18:57:39 +0530

    Repetation Practice problemwith while loop

--- factorial.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')();
-
-const number = parseInt(prompt('Enter the number: '));
-let fact = 1;
-
-for ( i = 2; i <= number; i++ ) {
-    fact = fact * i;
-}
-console.log(`Factorial of the ${number} is : ` +fact);
---
+++ powerOfTwoSwitch.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+const number = prompt('Enter the value of n: ');
+
+i=0
+while ( i <= number ) {
+    let powerOf2 = Math.pow(2, i);
+    if (powerOf2 <= 256) {
+        console.log(powerOf2);
+        i++;
+    }
+    else {
+        break;
+    }
+}
